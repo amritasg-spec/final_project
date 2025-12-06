4b13282e1a75f84f76c908faf045cf40a6d2ca32
wmtonelli <wtonelli@umich.edu>
2025-12-06 14:16:39 -0500

    ..

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sqlite3
 from edamam_api import get_edamam_nutrition, store_edamam_nutrition
 from kroger_api import get_kroger_products
-from mealdb_api import create_meal_tables
+from mealdb_api import create_meal_tables, store_meal
 
 def main():
     conn = sqlite3.connect("final_project.db")
