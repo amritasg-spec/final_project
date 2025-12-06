53edc385a7d112bbef6bbfef13ef319aef130d95
Amrita Gujarati <amritagujarati@Amritas-MacBook-Air-7.local>
2025-12-06 12:10:27 -0500

    added those updated variables into the table

--- main.py
+++ main.py
@@ -8,19 +8,24 @@ def main():
 
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS meal_nutrition (
-            meal_id INTEGER PRIMARY KEY,
-            calories REAL,
-            protein REAL,
-            fat REAL,
-            carbs REAL,
-            sugar REAL,
-            fiber REAL,
-            sodium REAL,
-            diet_labels TEXT,
-            health_labels TEXT
+        meal_id INTEGER PRIMARY KEY,
+        calories REAL,
+        protein REAL,
+        fat REAL,
+        carbs REAL,
+        sugar REAL,
+        fiber REAL,
+        sodium REAL,
+        diet_labels TEXT,
+        health_labels TEXT,
+        cuisineType TEXT,
+        mealType TEXT,
+        dishType TEXT
         );
     """)
+    
 
+        
     ingredients = ["2 eggs", "1 cup milk", "1 tbsp sugar"]
     nutrition = get_edamam_nutrition(ingredients)
     store_edamam_nutrition(cursor, 1, nutrition)
